91d22f8de24f43897dbf5355a8572b2052f0b0d7
Lizihmann <145053836+Lizihmann@users.noreply.github.com>
2023-09-14 12:48:55 -0600

    Create Coding_rush_code.py

---
+++ Coding_rush_code.py
@@ -0,0 +1,9 @@
+#coding rush code
+def dessert():
+  print("give me a word")
+  word = input()
+  print("give me a second word")
+  inverted_word = word[::-1]
+  print("bye, see you tomorrow!")
+
+dessert()
