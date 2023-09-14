e5037edcc81404b7a1994c42f5e095684bfaa55d
Lizihmann <145053836+Lizihmann@users.noreply.github.com>
2023-09-14 12:53:30 -0600

    Create Exercises_rectangle.py

---
+++ Exercises_rectangle.py
@@ -0,0 +1,12 @@
+#exercise rectangle
+def name():
+  print("hello, how are you?")
+  variable = input()
+  print("what's the length of the triangle?")
+  length = input()
+  print("and what's the width of the triangle?")
+  width = input()
+  result = int(length)*int(width)
+  print("that's the area of the rectangle", result)
+
+name()
