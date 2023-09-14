13d5a17a17c10b4c849ad2165993d96b5e16faef
Lizihmann <145053836+Lizihmann@users.noreply.github.com>
2023-09-14 12:50:27 -0600

    Create Exercises_tips.py

---
+++ Exercises_tips.py
@@ -0,0 +1,10 @@
+#exercises tips
+def name():
+  print ("have you eaten today?")
+  variable = input()
+  print("what did you pay?")
+  number = input()
+  result = int(number)*.20
+  print("your tip is", result)
+
+name()
